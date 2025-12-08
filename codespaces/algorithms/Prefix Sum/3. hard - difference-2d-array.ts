0abc952e254129db1284d3c1cb9f65630c8c1e89
Wan <vuquangkhtn@gmail.com>
2025-12-08 07:57:03 +0700

    add prefix sum prb

--- codespaces/algorithms/Prefix Sum/3. hard - difference-2d-array.ts
+++ codespaces/algorithms/Prefix Sum/3. hard - difference-2d-array.ts
@@ -1,1 +1,2 @@
-// https://www.youtube.com/watch?v=NL7g-VRFXtQ
+// Explaination: https://www.youtube.com/watch?v=NL7g-VRFXtQ
+// Problem: https://leetcode.com/problems/count-the-number-of-substrings-with-dominant-ones/description/
